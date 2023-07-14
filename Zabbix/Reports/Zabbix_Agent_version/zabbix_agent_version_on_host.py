65ad89e04243965cd91fff5dce690a0ffebca9f3
Vermiz <95149215+Vermiz@users.noreply.github.com>
2023-07-14 15:51:20 +0200

    Update zabbix_agent_version_on_host.py

--- Zabbix/Reports/Zabbix_Agent_version/zabbix_agent_version_on_host.py
+++ Zabbix/Reports/Zabbix_Agent_version/zabbix_agent_version_on_host.py
@@ -7,7 +7,7 @@
 # in this case I check if the zabbix agent is up to date.
 #
 # Requirements: pip install pyzabbix
-# Rember to change zabbix IP server, login, password and host group.
+# Remember to change zabbix IP server, login, password and host group if you want check another item, change value for item_key.
 #####
 
 from pyzabbix import ZabbixAPI
